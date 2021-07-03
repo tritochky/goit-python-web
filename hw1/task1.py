28bf0928cf7d6efe2fc5a1734b354163aef8512b
Olga Fomenko <helga.fomenko@gmail.com>
2021-07-03 22:22:25 +0300

    hw1

---
+++ hw1/task1.py
@@ -0,0 +1,150 @@
+﻿import json
+import pickle
+from abc import abstractmethod, ABC
+
+
+class SerializationInterface(ABC):
+    @abstractmethod
+    def serialize(self):
+        pass
+
+    @abstractmethod
+    def deserialize(self):
+        pass
+
+
+class ListJsonSerialization(SerializationInterface):
+    def __init__(self, data, file_name):
+        self.data = []
+        self.file_name = ''
+
+    def serialize(self):
+        with open(self.file_name, "w") as fh:
+            json.dump(self.data, fh)
+            return 'Done!'
+
+    def deserialize(self):
+        with open(self.file_name, "r") as fh:
+            unpacked = json.load(fh)
+            assert self.data == unpacked, 'Wrong deserialization!'
+            return unpacked
+        
+
+class DictJsonSerialization(SerializationInterface):
+    def __init__(self, data, file_name):
+        self.data = {}
+        self.file_name = ''
+
+    def serialize(self):
+        with open(self.file_name, "w") as fh:
+            json.dump(self.data, fh)
+            return 'Done!'
+
+    def deserialize(self):
+        with open(self.file_name, "r") as fh:
+            unpacked = json.load(fh)
+            assert self.data == unpacked, 'Wrong deserialization!'
+            return unpacked
+
+
+class TupleJsonSerialization(SerializationInterface):
+    def __init__(self, data, file_name):
+        self.data = ()
+        self.file_name = ''
+
+    def serialize(self):
+        with open(self.file_name, "w") as fh:
+            json.dump(self.data, fh)
+            return 'Done!'
+
+    def deserialize(self):
+        with open(self.file_name, "r") as fh:
+            unpacked = tuple(json.load(fh))
+            assert self.data == unpacked, 'Wrong deserialization!'
+            return unpacked
+
+
+class SetJsonSerialization(SerializationInterface):
+    def __init__(self, data, file_name):
+        self.data = set()
+        self.file_name = ''
+
+    def serialize(self):
+        self.data = list(self.data)
+        with open(self.file_name, "w") as fh:
+            json.dump(self.data, fh)
+            return 'Done!'
+
+    def deserialize(self):
+        with open(self.file_name, "r") as fh:
+            unpacked = set(json.load(fh))
+            assert self.data == unpacked, 'Wrong deserialization!'
+            return unpacked
+
+
+class ListPicklSerialization(SerializationInterface):
+    def __init__(self, data, file_name):
+        self.data = []
+        self.file_name = ''
+
+    def serialize(self):
+        with open(self.file_name, "wb") as fh:
+            pickle.dump(self.data, fh)
+            return 'Done!'
+
+    def deserialize(self):
+        with open(self.file_name, "rb") as fh:
+            unpacked = pickle.load(fh)
+            assert self.data == unpacked, 'Wrong deserialization!'
+            return unpacked
+
+
+class DictPicklSerialization(SerializationInterface):
+    def __init__(self, data, file_name):
+        self.data = {}
+        self.file_name = ''
+
+    def serialize(self):
+        with open(self.file_name, "wb") as fh:
+            pickle.dump(self.data, fh)
+            return 'Done!'
+
+    def deserialize(self):
+        with open(self.file_name, "rb") as fh:
+            unpacked = pickle.load(fh)
+            assert self.data == unpacked, 'Wrong deserialization!'
+            return unpacked
+
+
+class TuplePicklSerialization(SerializationInterface):
+    def __init__(self, data, file_name):
+        self.data = ()
+        self.file_name = ''
+
+    def serialize(self):
+        with open(self.file_name, "wb") as fh:
+            pickle.dump(self.data, fh)
+            return 'Done!'
+
+    def deserialize(self):
+        with open(self.file_name, "rb") as fh:
+            unpacked = pickle.load(fh)
+            assert self.data == unpacked, 'Wrong deserialization!'
+            return unpacked
+
+
+class SetPicklSerialization(SerializationInterface):
+    def __init__(self, data, file_name):
+        self.data = {}
+        self.file_name = ''
+
+    def serialize(self):
+        with open(self.file_name, "wb") as fh:
+            pickle.dump(self.data, fh)
+            return 'Done!'
+
+    def deserialize(self):
+        with open(self.file_name, "rb") as fh:
+            unpacked = pickle.load(fh)
+            assert self.data == unpacked, 'Wrong deserialization!'
+            return unpacked
---
+++ hw1/task2.py
@@ -0,0 +1,27 @@
+﻿class Meta(type):
+    def __new__(self, name, bases, attrs):
+        attrs['class_number'] = self.children_number 
+        self.children_number +=1
+        return type.__new__(self, name, bases, attrs)
+
+    def __init__(self, name, bases, attrs):
+        super().__init__(name, bases, attrs)
+
+    def __call__(self, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
+
+
+Meta.children_number = 0
+
+class Cls1(metaclass=Meta):
+    def __init__(self, data):
+        self.data = data
+        
+
+class Cls2(metaclass=Meta):
+    def __init__(self, data):
+        self.data = data
+
+assert (Cls1.class_number, Cls2.class_number) == (0, 1)
+a, b = Cls1(''), Cls2('')
+assert (a.class_number, b.class_number) == (0, 1)
